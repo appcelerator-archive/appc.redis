5427cc967985c3f5f4c0895475662c1e71ff4097
Isaac Whitfield <iwhitfield@appcelerator.com>
2015-09-24 16:06:48 +0100

    Update Grunt tasks and dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,17 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-		mochacov: {
-			options: {
-				slow: 1000,
-				timeout: 3000,
-				reporter: 'html-cov',
-				output: 'coverage/index.html'
-			},
-			all: ['test/**/*.js']
+		mocha_istanbul:{
+			coverage: {
+				print: 'none',
+				quiet: true,
+				excludes: [
+					'**/node_modules/**'
+				],
+				src: [
+					'test/**/*.js'
+				]
+			}
 		},
 		mochaTest: {
 			options: {
@@ -26,17 +29,23 @@ module.exports = function(grunt) {
 			},
 			src: ['*.js','lib/**/*.js','models/**/*.js','test/**/*.js']
 		},
-		clean: ['coverage','logs','tmp']
+		clean: {
+			coverage: ['coverage'],
+			logs: ['logs'],
+			tmp: ['tmp']
+		}
 	});
 
 	// Load grunt plugins for modules
-	grunt.loadNpmTasks('grunt-mocha-cov');
+	grunt.loadNpmTasks('grunt-mocha-istanbul');
 	grunt.loadNpmTasks('grunt-mocha-test');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	// register tasks
-	grunt.registerTask('coverage', ['mochacov']);
-	grunt.registerTask('default', ['jshint', 'mochaTest', 'clean']);
+	grunt.registerTask('coverage', ['clean:coverage','mocha_istanbul:coverage']);
+	grunt.registerTask('default', ['lint','test','clean']);
+	grunt.registerTask('lint', ['jshint']);
+	grunt.registerTask('test', ['mochaTest']);
 
 };
