e7e89a94511334971e973abcc2bc8a6d20f8133b
Aleksandrina Karakehayova <akarakehayova@axway.com>
2017-03-21 14:40:43 +0200

    Added server logic

--- app.js
+++ app.js
@@ -3,16 +3,16 @@
  * be used or packaged with the actual connector when published.
  */
 var Arrow = require('arrow'),
-	server = new Arrow();
+  server = new Arrow()
 
 // lifecycle examples
 server.on('starting', function () {
-	server.logger.debug('server is starting!');
-});
+  server.logger.debug('server is starting!')
+})
 
 server.on('started', function () {
-	server.logger.debug('server started!');
-});
+  server.logger.debug('server started!')
+})
 
 // create some users programmatically
 var users = [
@@ -22,17 +22,17 @@ var users = [
 	{name: 'Vasil'},
 	{name: 'Rick'},
 	{name: 'Dawson'}
-];
+]
 
 // start the server
 server.start(function () {
-	var User = Arrow.Model.extend('appc.redis/base', 'user', {
-		fields: {
-			name: {type: String, required: false, validator: /[a-zA-Z]{3,}/}
-		}
-	});
+  var User = Arrow.Model.extend('appc.redis/base', 'user', {
+    fields: {
+      name: {type: String, required: false, validator: /[a-zA-Z]{3,}/}
+    }
+  })
 
-	User.create(users, function (err, users) {
-		server.logger.info('Created some users', users);
-	});
-});
+  User.create(users, function (err, users) {
+    server.logger.info('Created some users', users)
+  })
+})
---
+++ test/server.js
@@ -0,0 +1,39 @@
+'use strict'
+const Arrow = require('arrow')
+
+module.exports = function (options) {
+  return new Promise((resolve, reject) => {
+    options = options || {}
+    const arrow = new Arrow({}, true)
+    const connector = arrow.getConnector('appc.redis')
+    connector.metadata = {}
+
+    if (options.generateTestModels !== false) {
+            // Create test model - testModel
+      arrow.addModel(Arrow.createModel('testModel', {
+        name: 'testModel',
+        connector,
+        fields: {
+          fname: {
+            type: 'string',
+            required: false
+          },
+          lname: {
+            type: 'string',
+            required: false
+          },
+          age: {
+            type: 'number',
+            required: false
+          }
+        },
+        metadata: {
+          primarykey: 'id'
+        }
+      }))
+    }
+
+        // Return the arrow instance
+    resolve(arrow)
+  })
+}
